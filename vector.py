1322f4ef27496b44f413ca1df6be7c3b5d1f4836
Juul Christiaens <juul@MacBook-Pro-van-Juul.local>
2022-09-12 10:46:18 +0200

    cleaned up some funcionality of the vectors

--- vector.py
+++ vector.py
@@ -2,17 +2,8 @@ import math
 
 def vector_input():
 
-    v1 = []
-    v2 = []
-
-    vector1 = input("first vector: ")
-    vector2 = input("second vector: ")
-
-    for i in vector1:
-        v1.append(int(i))
-
-    for i in vector2:
-        v2.append(int(i))
+    v1 = input("first vector: ")
+    v2 = input("second vector: ")
 
     print("What do you want to do with these vectors?")
     n = input("choose from sum, dot-product,... ") # the three dots need to be filled in when new features are added
@@ -27,12 +18,12 @@ def vector_input():
 def vector_sum(v1, v2):
     sum_v = []
     for i in range(len(v1)):
-        x = v1[i] + v2[i]
+        x = int(v1[i]) + int(v2[i])
         sum_v.append(x)
     print(sum_v)
 
 def vector_length(v1, v2):
-    pass
+    length = math.sqrt()
 
 def angle(v1, v2):
     pass
